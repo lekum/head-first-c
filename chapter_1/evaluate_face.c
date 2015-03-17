9b84bdfb13dd6081cdde18b7c4e8ba566feacbaf
Alejandro Guirao <lekumberri@gmail.com>
2015-03-17 11:13:44 +0100

    Change logic to switch

--- chapter_1/evaluate_face.c
+++ chapter_1/evaluate_face.c
@@ -12,16 +12,19 @@ int main()
     puts("Enter the card_name: ");
     scanf("%2s", card_name);
     int val = 0;
-    if (card_name[0] == 'K')
-        val = 10;
-    else if (card_name[0] == 'Q')
-        val = 10;
-    else if (card_name[0] == 'J')
-        val = 10;
-    else if (card_name[0] == 'A')
-        val = 11;
-    else
-        val = atoi(card_name);
+    switch (card_name[0])
+    {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        default:
+            val = atoi(card_name); 
+    }
     if ( (val > 2) && (val < 7) )
         puts("Count has gone up");
     else if (val == 10)
