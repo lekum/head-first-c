5785772ad451c09b07ae32ddf33398c2af63e4f4
Alejandro Guirao <lekumberri@gmail.com>
2015-03-23 09:40:02 +0100

    Working version with pointers

--- chapter_2/bermuda_rectangle/bermuda_rectangle.c
+++ chapter_2/bermuda_rectangle/bermuda_rectangle.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 
-void go_south_east(int lat, int lon)
+void go_south_east(int *lat, int *lon)
 {
-    lat = lat - 1;
-    lon = lon + 1;
+    *lat = *lat - 1;
+    *lon = *lon + 1;
 }
 
 int main()
 {
     int latitude = 32;
     int longitude = -64;
-    go_south_east(latitude, longitude);
+    go_south_east(&latitude, &longitude);
+    printf("The memory addresses are: [%p, %p]\n", &latitude, &longitude);
+    printf("The size of an int is: %li\n", sizeof(latitude));
     printf("Avast! Now at: [%i, %i]\n", latitude, longitude);
     return 0;
 }
