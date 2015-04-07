7ddec24ba808273d4116723e75624d4f5cb74f7f
Alejandro Guirao <lekumberri@gmail.com>
2015-04-07 16:24:38 +0200

    Modify test

--- chapter_2.5/test_strstr/test_strstr.c
+++ chapter_2.5/test_strstr/test_strstr.c
@@ -4,11 +4,11 @@
 int main(int argc, char **argv)
 {
     char *pos;
-    char s0[] = "dysfunctional";
+    char s0[][80] = {"dysfunctional", "troll"};
     char s1[] = "fun";
-    if (pos = strstr(s0, s1))
+    if (pos = strstr(s0[0], s1))
     {
-        printf("The string \"%s\" has been found inside \"%s\"\n", s1, s0);
+        printf("The string \"%s\" has been found inside \"%s\"\n", s1, s0[0]);
         return 0;
     }
     return -1;
