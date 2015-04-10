7e29b77b0c685d2841daccd25225f4e6f96d7ddb
Alejandro Guirao <lekumberri@gmail.com>
2015-04-10 09:45:41 +0200

    Geo2json improved

--- chapter_3/geo2json/geo2json.c
+++ chapter_3/geo2json/geo2json.c
@@ -14,9 +14,21 @@ int main()
             printf(",\n");
         else
             started = 1;
+
+        if ( (latitude < -90) || (latitude > 90) )
+        {
+            printf("Invalid latitude: %f\n", latitude);
+            return 2;
+        }
+        if ( (longitude < -180) || (longitude > 180) )
+        {
+            printf("Invalid longitude: %f\n", longitude);
+            return 2;
+        }
+
         printf("{latitude: %f, longitude: %f, info: '%s'}",
                 latitude, longitude, info);
     }
-    puts("\n");
+    puts("\n]");
     return 0;
 }
