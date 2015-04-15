cb5f237360898317118e068315ed180fdaf96b3e
Alejandro Guirao <lekumberri@gmail.com>
2015-04-15 10:06:41 +0200

    Add categorize

---
+++ chapter_3/categorize/categorize.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    char line[80];
+    if (argc != 6)
+    {
+        fprintf(stderr, "You need to give 5 arguments\n");
+        return -1;
+    }
+    FILE *in;
+    if (!(in= fopen("spooky.csv", "r")))
+    {
+        fprintf(stderr, "Can't open the input file 'spooky.csv'\n");
+        return 1;
+    }
+    FILE *file1 = fopen(argv[2], "w");
+    FILE *file2 = fopen(argv[4], "w");
+    FILE *file3 = fopen(argv[5], "w");
+    while (fscanf(in, "%79[^\n]\n", line) == 1)
+    {
+        if (strstr(line, argv[1]))
+            fprintf(file1, "%s\n", line);
+        else if (strstr(line, argv[3]))
+            fprintf(file2, "%s\n", line);
+        else
+            fprintf(file3, "%s\n", line);
+    }
+    fclose(file1);
+    fclose(file2);
+    fclose(file3);
+    return 0;
+}
