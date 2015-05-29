d0e4cbe52800bb3f316dcdf4c0f9cec5b1725155
Alejandro Guirao <lekumberri@gmail.com>
2015-05-29 10:54:07 +0200

    Change to pipes

--- chapter_10/stories/stories.c
+++ chapter_10/stories/stories.c
@@ -9,38 +9,40 @@ void error(char *msg)
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
 }
 
+void open_url(char *url)
+{
+    char command[255];
+    sprintf(command, "x-www-browser '%s'", url);
+    system(command);
+}
+
 int main(int argc, char *argv[])
 {
     char *phrase = argv[1];
     char *vars[] = {"RSS_FEED=http://feeds.feedburner.com/planetarduino?format=xml", NULL};
-    FILE *f = fopen("stories.txt", "w");
-    if (!f)
-    {
-        error("Can't open stories.txt");
-    }
+    int fd[2];
+    if (pipe(fd) == -1)
+        error("Can't create the pipe");
+
     pid_t pid = fork();
     if (pid == -1)
-    {
         error("Can't fork process");
-    }
     if (!pid)
     {
-        if (dup2(fileno(f), 1) == -1)
-        {
+        close(fd[0]); // Close the "read" end of the pipe
+        if (dup2(fd[1], 1) == -1)
             error("Can't redirect Standard Output");
-        }
-        if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py",
-                   phrase, NULL, vars) == -1)
-        {
+        if (execle("/usr/bin/python", "/usr/bin/python", "./rssgossip.py", "-u", phrase, NULL, vars) == -1)
             error("Can't run script");
-        }
     }
-    int pid_status;
-    if (waitpid(pid, &pid_status, 0) == -1)
+    close(fd[1]); // Close the "write" end of the pipe
+    if (dup2(fd[0], 0) == -1)
+        error("Can't redirect Standard Input");
+    char line[255];
+    while (fgets(line, 255, stdin))
     {
-        error("Error waiting for child process");
+        if (line[0] == '\t') // If the line starts with a tab
+            open_url(line + 1);
     }
-    if (WEXITSTATUS(pid_status))
-        puts("Error status non-zero");
     return 0;
 }
